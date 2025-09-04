01d582143add04437469e5505c291b98648426e6
Joao Victor <peixoto.jvs@gmail.com>
2025-09-04 17:32:23 -0300

    feat: conexão local com o banco de dados MySql

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DespesaModule } from './despesa/despesa.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { DespesaModule } from './despesa/despesa.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [DespesaModule, UsuarioModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'comercio',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+      dropSchema: true,
+    }),
+    DespesaModule,
+    UsuarioModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
