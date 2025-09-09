2cc09955b191bcd0b6a63accb68fd499ec02a328
ricardoiwata <iwataricardo1@gmail.com>
2025-09-09 15:11:40 -0300

    Atualização de segurança, remoção de chaves de api

--- frontend/src/auth/firebase.ts
+++ frontend/src/auth/firebase.ts
@@ -6,15 +6,26 @@ import {
     initializeAuth
 } from 'firebase/auth';
 
+import {
+    API_KEY,
+    APP_ID,
+    AUTH_DOMAIN,
+    MEASUREMENT_ID,
+    MESSAGING_SENDER_ID,
+    PROJECT_ID,
+    STORAGE_BUCKET,
+} from 'react-native-dotenv';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyC3XfuMuAJwfZkWvUurDNQ0hc2QC113p4E",
-    authDomain: "conta-certa-fbc0d.firebaseapp.com",
-    projectId: "conta-certa-fbc0d",
-    storageBucket: "conta-certa-fbc0d.firebasestorage.app",
-    messagingSenderId: "185577603360",
-    appId: "1:185577603360:web:a9f8e6c5fd5cf2411fd745",
-    measurementId: "G-H80E0TYJR4"
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID,
+    measurementId: MEASUREMENT_ID,
 };
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app, {
